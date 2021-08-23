9551a08c5440486d9740882245881b32505574e2
Rochim <rochim.rci26@gmail.com>
2021-08-23 14:40:25 +0700

    initial project

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import "../styles/globals.css";
+import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+          crossorigin="anonymous"
+        />
+
+        {/* Font */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <NextNProgress
+        color="#96DAFF"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="4"
+      />
+      <Component {...pageProps} />
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+        crossorigin="anonymous"
+      ></script>
+    </>
+  );
+}
+
+export default MyApp;
