ada27a10700a287a437a87173f81e6c686473fe2
alifpramana <alif.pramp@gmail.com>
2021-08-25 22:52:09 +0700

    update cari sekolah?search

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import SelectShared from "./components/Shared/SelectShared/SelectShared";
 import SearchCariSekolah from "./components/SearchCariSekolah/SearchCariSekolah";
+import { FaFilter } from "react-icons/fa";
 
 export default function Home({ nav, search }) {
   const router = useRouter();
@@ -59,18 +60,100 @@ export default function Home({ nav, search }) {
                   </h6>
                 </div>
               </div>
-              <div className="pt-4 mb-5 ">
+              <div className="pt-2 mb-5 ">
                 <SearchCariSekolah nav={nav} />
               </div>
-              <div
-                className="row pt-5 position-relative gy-4 search-rpp-content"
-                style={{ zIndex: "50" }}
-              >
-                <div className="col-md-12 text-center mb-4">
-                  <h2 className="color-dark fw-extrabold">SMK</h2>
-                  <p className="fw-semibold">
-                    1000 sekolah untuk kata kunci SMK
-                  </p>
+              <div className="row pt-5 mt-4 position-relative gy-4 search-rpp-content justify-content-center">
+                <div className="col-sm-11 mb-4 py-md-3 py-sm-5 py-3 px-sm-0 mt-md-0 mt-sm-4 mt-0 ">
+                  <div className="row mb-4">
+                    <div className="col-md-12 d-flex justify-content-between align-items-md-center flex-md-row flex-column">
+                      <p className="fw-semibold mb-md-0 mt-md-0 mt-3">
+                        Menampilkan 1 - 25 sekolah dari total 250 untuk kata
+                        kunci
+                        {""}
+                        <span className="color-dark fw-bold"> "SMK"</span>
+                      </p>
+                      <button
+                        className={`btn py-2 px-4 btn-collapse-filter btn-light rounded-pill border bg-white color-dark fw-bold ${
+                          collapseOpen && "active"
+                        }`}
+                        data-bs-toggle="collapse"
+                        href="#collapseExample"
+                        role="button"
+                        aria-expanded="false"
+                        aria-controls="collapseExample"
+                        onClick={() => setcollapseOpen(!collapseOpen)}
+                        data-joyride="filter-perpus-collapse"
+                      >
+                        <FaFilter className="me-3 fs-14-ss" />
+                        Filter
+                      </button>
+                    </div>
+                  </div>
+                  <div className="collapse" id="collapseExample">
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <label className="form-label">Provinsi</label>
+                        <div className="form-select-cari-sekolah">
+                          <SelectShared
+                            name="provinsi"
+                            placeholder="Pilih provinsi"
+                            // // handleChangeSelect={handleChangeSelect}
+                            // value={formData.agama}
+                            // options={agamaData}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <label className="form-label">Kabupaten / Kota</label>
+                        <div className="form-select-cari-sekolah">
+                          <SelectShared
+                            name="kabupatenKota"
+                            placeholder="Pilih kabupaten / kota"
+                            // // handleChangeSelect={handleChangeSelect}
+                            // value={formData.agama}
+                            // options={agamaData}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="d-flex align-items-center">
+                        <span className="btn-ss rounded-pill bg-soft-secondary color-secondary fw-bold me-4">
+                          DKI Jakarta
+                        </span>
+                        <span className="color-primary fw-bold me-4 pointer">
+                          Hapus Semua
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <hr className="my-4" />
+                  <div className="card card-ss w-100 px-4 py-3">
+                    <div className="d-flex align-items-sm-center justify-content-between flex-sm-row flex-column">
+                      <div className="d-flex mb-sm-0 mb-3">
+                        <img
+                          src="/img/logo-26.png"
+                          alt="logo-sekolah"
+                          className="img-fit-contain me-3"
+                          style={{ width: "60px", height: "60px" }}
+                        />
+                        <div className="">
+                          <h4 className="fw-extrabold color-dark mb-2">
+                            SMKN 26 Jakarta
+                          </h4>
+                          <span className="mb-0">Jakarta</span>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-end">
+                        <a href="#">
+                          <img src="/img/icon-link.svg" alt="icon" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
