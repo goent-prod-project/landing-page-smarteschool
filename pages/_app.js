2de9c43379ab1d73012aa5d278622a72b62883e7
ihsannur <ihsannurraihan@gmail.com>
2024-05-02 11:14:50 +0700

    fix cdn bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
           crossorigin="anonymous"
@@ -66,7 +66,7 @@ function MyApp({ Component, pageProps }) {
         <Toaster position="top-right" reverseOrder={false} />
       </AnimatePresence>
       <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js"
         integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
         crossorigin="anonymous"
       ></script>
